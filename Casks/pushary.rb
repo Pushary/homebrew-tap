f7ab346558db29363fab818087e14fefa9c7c67f
pushary-release <business@pushary.com>
2026-09-02 17:42:11 +0000

    pushary 0.1.15

--- Casks/pushary.rb
+++ Casks/pushary.rb
@@ -1,6 +1,6 @@
 cask "pushary" do
-  version "0.1.14"
-  sha256 "a066caf9656f58f7867b9774c5a283718289795985403b3009d274b5ea916b05"
+  version "0.1.15"
+  sha256 "c381932682205fdcf533aa33261e9ddda9abc6426b544612c98e88eb42c92246"
 
   url "https://github.com/Pushary/pushary-mac/releases/download/v#{version}/Pushary.dmg"
   name "Pushary"
